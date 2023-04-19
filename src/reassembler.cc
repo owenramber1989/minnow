bee24db371842b03c1bdc8d8a5f64142446e5a67
owenramber1989 <owenramber1989@163.com>
2023-04-19 15:00:29 +0800

    temp change

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -1,18 +1,15 @@
 #include "reassembler.hh"
+#include <cstdint>
 
 using namespace std;
 
-void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring, Writer& output )
-{
-  // Your code here.
-  (void)first_index;
-  (void)data;
-  (void)is_last_substring;
-  (void)output;
+void Reassembler::insert(uint64_t first_index, string data,
+                         bool is_last_substring, Writer &output) {
+    if (first_index > nxt_idx + output.available_capacity())
+        return;
+    if (is_last_substring) {
+        if (i)
+    }
 }
 
-uint64_t Reassembler::bytes_pending() const
-{
-  // Your code here.
-  return {};
-}
+uint64_t Reassembler::bytes_pending() const { return {}; }
