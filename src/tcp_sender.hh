4a31f55fb8db005999253f40ea7f58132b6b6e78
owenramber1989 <owenramber1989@163.com>
2023-04-27 21:40:07 +0800

    stuck on retx twice test, due to empty _message

--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -3,32 +3,64 @@
 #include "byte_stream.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
+#include <cstdint>
 
-class TCPSender
-{
-  Wrap32 isn_;
-  uint64_t initial_RTO_ms_;
+class Timer {
+  private:
+    uint64_t ticks_ = 0;
+    bool running = false;
 
-public:
-  /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
-  TCPSender( uint64_t initial_RTO_ms, std::optional<Wrap32> fixed_isn );
+  public:
+    bool expired(uint64_t ms_since_last_tick, uint64_t time_out_) {
+        ticks_ += ms_since_last_tick;
+        return running && (ticks_ >= time_out_);
+    }
+    bool in_run() { return running; }
+    void start() {
+        ticks_ = 0;
+        running = true;
+    }
+    void stop() { running = false; }
+};
+class TCPSender {
+    Wrap32 isn_;
+    uint64_t initial_RTO_ms_;
+    uint64_t consecutive_retransmissions_ = 0;
+    uint64_t rcvno = 0;
+    uint64_t nxt_seqno_ = 0;
+    uint64_t window_size_ = 1;
+    uint64_t retransmission_timeout_ = initial_RTO_ms_;
+    uint64_t bytes_in_flight_ = 0;
+    bool syn_sent_ = false;
+    bool fin_sent_ = false;
+    Timer timer_{};
+    std::queue<TCPSenderMessage> _messages{};
+    std::queue<TCPSenderMessage> _outstanding_messages{};
 
-  /* Push bytes from the outbound stream */
-  void push( Reader& outbound_stream );
+  public:
+    /* Construct TCP sender with given default Retransmission Timeout and
+     * possible ISN */
+    TCPSender(uint64_t initial_RTO_ms, std::optional<Wrap32> fixed_isn);
 
-  /* Send a TCPSenderMessage if needed (or empty optional otherwise) */
-  std::optional<TCPSenderMessage> maybe_send();
+    /* Push bytes from the outbound stream */
+    void push(Reader &outbound_stream);
 
-  /* Generate an empty TCPSenderMessage */
-  TCPSenderMessage send_empty_message() const;
+    /* Send a TCPSenderMessage if needed (or empty optional otherwise) */
+    std::optional<TCPSenderMessage> maybe_send();
 
-  /* Receive an act on a TCPReceiverMessage from the peer's receiver */
-  void receive( const TCPReceiverMessage& msg );
+    /* Generate an empty TCPSenderMessage */
+    TCPSenderMessage send_empty_message() const;
 
-  /* Time has passed by the given # of milliseconds since the last time the tick() method was called. */
-  void tick( uint64_t ms_since_last_tick );
+    /* Receive an act on a TCPReceiverMessage from the peer's receiver */
+    void receive(const TCPReceiverMessage &msg);
 
-  /* Accessors for use in testing */
-  uint64_t sequence_numbers_in_flight() const;  // How many sequence numbers are outstanding?
-  uint64_t consecutive_retransmissions() const; // How many consecutive *re*transmissions have happened?
+    /* Time has passed by the given # of milliseconds since the last time the
+     * tick() method was called. */
+    void tick(uint64_t ms_since_last_tick);
+
+    /* Accessors for use in testing */
+    uint64_t sequence_numbers_in_flight()
+        const; // How many sequence numbers are outstanding?
+    uint64_t consecutive_retransmissions()
+        const; // How many consecutive *re*transmissions have happened?
 };
